1efd93fa44032c1cbdb2d34874a07b050167ebcb
Vik <vvang044@gmail.com>
2021-06-13 21:33:16 -0400

    working with state in expenseItem.js

--- components/Expenses/ExpenseItem.js
+++ components/Expenses/ExpenseItem.js
@@ -1,3 +1,5 @@
+import React, {useState} from 'react';
+
 import Card from '../UI/Card';
 import ExpenseDate from './ExpenseDate';
 import './ExpenseItem.css';
@@ -10,8 +12,14 @@ function ExpenseItem(props){
     // const month = props.date.toLocaleString('en-US', { month: 'long' });
     // const year = props.date.getFullYear();
 
+    //array desctructuring [ ]
+   const [title, setTitle] = useState(props.title);
+
+   
+    
     const clickHandler = () =>{
-        console.log('Clicked!!!!');
+        setTitle('Updated!');
+        console.log(title);
     }
 
     return (
@@ -22,7 +30,8 @@ function ExpenseItem(props){
                
             </div>
             <div className="expense-item__description">
-                <h2>{props.title}</h2>
+                {/* <h2>{props.title}</h2> */}
+                <h2>{title}</h2>
             <div className="expense-item__price">${props.amount}</div>
             </div>
             <button onClick={clickHandler}>Change Title </button>
