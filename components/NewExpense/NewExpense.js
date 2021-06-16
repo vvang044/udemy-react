fc323c04839386cd83aa9f420eb4a04754d4afe4
Vik <vvang044@gmail.com>
2021-06-15 20:58:05 -0400

    listening to user input

--- components/NewExpense/NewExpense.js
+++ components/NewExpense/NewExpense.js
@@ -4,6 +4,7 @@ import './NewExpense.css';
 
 const NewExpense = () =>{
     return (
+            // <form></form> moved to ExpenseForm.js
             <div className="new-expense">
                 <ExpenseForm />
             </div>
