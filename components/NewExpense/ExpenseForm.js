222a25795a69605e6b334d764015fea8ef0cca36
Vik <vvang044@gmail.com>
2021-06-16 21:09:03 -0400

    two way binding with form submission

--- components/NewExpense/ExpenseForm.js
+++ components/NewExpense/ExpenseForm.js
@@ -61,6 +61,9 @@ const ExpenseForm = () => {
         }
         //to test console.log the expenseData
         console.log(expenseData);
+        setEnteredTitle('');
+        setEnteredAmount('');
+        setEnteredDate('');
     };
 
     return (
@@ -68,15 +71,29 @@ const ExpenseForm = () => {
         <div className="new-expense__controls">
             <div className="new-expense__control">
                 <label>Title</label>
-                <input type='text' onChange={titleChangeHandler}/>
+                {/* two way binding by adding value attribute */}
+                <input 
+                    type='text' 
+                    value={enteredTitle} 
+                    onChange={titleChangeHandler}/>
             </div>
             <div className="new-expense__control">
                 <label>Amount</label>
-                <input type='number' min="0.01" step="0.01"onChange={amountChangeHandler}/>
+                <input 
+                    type='number' 
+                    min="0.01" 
+                    step="0.01" 
+                    value={enteredAmount}
+                    onChange={amountChangeHandler}/>
             </div>
             <div className="new-expense__control">
                 <label>Date</label>
-                <input type='date' min="2019-01-01" max="2022-12-31" onChange={dateChangeHandler}/>
+                <input 
+                    type='date' 
+                    min="2019-01-01" 
+                    max="2022-12-31" 
+                    value={enteredDate}
+                    onChange={dateChangeHandler}/>
             </div>
         </div>
         <div className="new-expense__actions">
