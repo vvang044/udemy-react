05d7a4c5a0e8946faebae651ef14f8b4791a6256
Vik <vvang044@gmail.com>
2021-08-01 17:49:03 -0400

    toggle add new expense and cancel button

--- components/NewExpense/NewExpense.js
+++ components/NewExpense/NewExpense.js
@@ -1,8 +1,11 @@
-import React from 'react';
+import React, {useState} from 'react';
 import ExpenseForm from './ExpenseForm';
 import './NewExpense.css';
 
 const NewExpense = (props) =>{
+   const [isEditing, setIsEditing] = useState(false);
+
+
     const saveExpenseDataHandler = (enteredExpenseData) =>{
         const expenseData = {
             ...enteredExpenseData,
@@ -10,14 +13,22 @@ const NewExpense = (props) =>{
         }
         // console.log(expenseData);
         props.onAddExpense(expenseData);
-
+        setIsEditing(false);
     }
 
+    const startEditingHandler = () => {
+        setIsEditing(true);
+    }
+
+    const stopEditingHandler = () => {
+        setIsEditing(false);
+    }
 
     return (
             // <form></form> moved to ExpenseForm.js
             <div className="new-expense">
-                <ExpenseForm onSaveExpenseData={saveExpenseDataHandler}/>
+                {!isEditing && <button onClick={startEditingHandler}>Add New Expense</button>}
+                {isEditing && <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} onCancel={stopEditingHandler}/>}
             </div>
             )
     }
